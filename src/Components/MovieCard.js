9427171d51831f8867c647f2b7d8101ffc146ea8
BhargavKalangi <91519470+BhargavKalangi@users.noreply.github.com>
2025-02-20 23:28:10 +0530

    shadow also added

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -3,7 +3,7 @@ import { IMG_URL } from "./Utils/constants";
 
 const MovieCard = ({ poster_path }) => {
   return (
-    <div className="w-48 pr-4 shadow-lg transition-all duration-700 hover:scale-110">
+    <div className="w-48 pr-4 shadow-lg transition-all duration-700 hover:scale-110 hover:shadow-md hover:shadow-gray-200">
       <img src={IMG_URL + poster_path} alt="Movie" />
     </div>
   );
